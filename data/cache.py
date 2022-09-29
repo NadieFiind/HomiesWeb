743d3b6af3702deb2ee727d2b1700fb4a859c325
NadieFiind <nadiefiind@gmail.com>
2022-09-30 00:33:36 +0800

    Add cache data lifetime

--- data/cache.py
+++ data/cache.py
@@ -1,19 +1,36 @@
+from datetime import datetime
 from data.models import Person
 from collections import OrderedDict
-from typing import Optional, TypeVar, Generic
+from typing import Optional, TypeVar, Generic, Dict
 
 Type = TypeVar("Type")
 
 
-class LimitedSizeDict(Generic[Type], OrderedDict[str, Type]):
+class CacheData(Generic[Type], OrderedDict[str, Type]):
 	def __init__(self, size_limit: int) -> None:
 		super().__init__()
 		self.size_limit = size_limit
+		self._lifetime: Dict[str, datetime] = {}
 		self._check_size_limit()
 	
 	def __setitem__(self, key: str, value: Type) -> None:
 		super().__setitem__(key, value)
+		self._lifetime[key] = datetime.now()
 		self._check_size_limit()
+	
+	def get(self, key: str) -> Optional[Type]:  # type: ignore
+		date_created = self._lifetime.get(key)
+		
+		if date_created is None:
+			return None
+		
+		# One hour lifetime
+		if (datetime.now() - date_created).total_seconds() >= 60 * 60:
+			del self._lifetime[key]
+			del self[key]
+			return None
+		
+		return super().get(key)
 	
 	def _check_size_limit(self) -> None:
 		while len(self) > self.size_limit:
@@ -22,7 +39,7 @@ class LimitedSizeDict(Generic[Type], OrderedDict[str, Type]):
 
 class DatabaseCache:
 	def __init__(self) -> None:
-		self.person = LimitedSizeDict[Person](1000)
+		self.person = CacheData[Person](1000)
 	
 	def get_person(self, person_id: str) -> Optional[Person]:
 		return self.person.get(person_id)
